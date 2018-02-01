c7bb926c161ce4cc594beaff015b9e0b6e04b3e5
logonrm <logonrm@fiap.com.br>
2018-01-31 22:46:33 -0200

    correcao

--- app/src/main/java/br/com/fiap/a21game/MainActivity.kt
+++ app/src/main/java/br/com/fiap/a21game/MainActivity.kt
@@ -2,32 +2,41 @@ package br.com.fiap.a21game
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import br.com.fiap.a21game.fragments.GameFragment
+import br.com.fiap.a21game.fragments.SobreFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.navigation_jogar -> {
+                changeFragment(GameFragment())
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+            R.id.navigation_sobre -> {
+                changeFragment(SobreFragment())
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+            R.id.navigation_sair -> {
+                finish()
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
 
+    fun changeFragment(fragment : Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frame, fragment).commit()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        changeFragment(GameFragment())
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
--- app/src/main/java/br/com/fiap/a21game/SplashActivity.kt
+++ app/src/main/java/br/com/fiap/a21game/SplashActivity.kt
@@ -1,7 +1,9 @@
 package br.com.fiap.a21game
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_splash.*
@@ -14,7 +16,7 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         ivLogo = findViewById(R.id.ivLogo)
-        
+
         carregar()
     }
 
@@ -22,5 +24,13 @@ class SplashActivity : AppCompatActivity() {
         val animacao = AnimationUtils.loadAnimation(this, R.anim.animacao_splash)
         ivLogo.clearAnimation()
         ivLogo.startAnimation(animacao)
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity,
+                    MainActivity::class.java)
+
+            startActivity(intent)
+            this@SplashActivity.finish()
+        }, 3000)
+
     }
 }
